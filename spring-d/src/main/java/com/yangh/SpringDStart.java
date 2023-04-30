93f4235c7fc1d628e12b7d1c119e033b404928d0
yangh <774283451@qq.com>
2023-04-30 11:51:59 +0800

    分支第一次操作

---
+++ spring-d/src/main/java/com/yangh/SpringDStart.java
@@ -0,0 +1,19 @@
+package com.yangh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author "杨航_2211"
+ * @Descript {
+ * 介绍：
+ * }
+ * @Project SpringCloud-Alibaba
+ * @date 2023/04/30 上午 11:38
+ */
+@SpringBootApplication
+public class SpringDStart {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDStart.class);
+    }
+}
